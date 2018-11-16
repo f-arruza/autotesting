58ef78c9db60c8237aa4fcaec3714fd46279b14d
f-arruza <f.arruza@uniandes.edu.co>
2018-11-16 16:01:29 -0500

    BASH de integración de contenedor de Docker Android_Builder con Android-Calabash instalado localmente

--- containers/run_mdroidplus.sh
+++ containers/run_mdroidplus.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Generar mutantes
-docker-compose -f docker-compose.mobile.yml run --rm mdroidplus
+# docker-compose -f docker-compose.mobile.yml run --rm mdroidplus
+
+# Se crea enlace simbólico a carpeta de Set de Pruebas de Calabash
+ln -s $(pwd $PATH)/src/calabash/features/ $(pwd $PATH)
 
 for folder in $(pwd $PATH)/src/mdroidplus/mutants/*
 do
@@ -9,6 +12,7 @@ do
   then
       FOLDER_NAME=`basename $folder`
       mkdir -p $(pwd $PATH)/src/mdroidplus/apks/$FOLDER_NAME/
+      mkdir -p $(pwd $PATH)/src/results/screenshots/calabash/$FOLDER_NAME/
 
       # Se crea enlace simbólico a la carpeta del mutante X
       ln -s $folder $(pwd $PATH)/src/android-builder
@@ -18,8 +22,18 @@ do
 
       find $(pwd $PATH)/src/android-builder/ -type f -name '*.apk' -exec cp {} $(pwd $PATH)/src/mdroidplus/apks/$FOLDER_NAME/ \;
 
+      # Firmar APK
+      find $(pwd $PATH)/src/mdroidplus/apks/$FOLDER_NAME/ -type f -name '*.apk' -exec calabash-android resign {} \;
+
+      # Ejecutar Set de pruebas BDT
+      export SCREENSHOT_PATH="$(pwd $PATH)/src/results/screenshots/calabash/$FOLDER_NAME/"
+      find $(pwd $PATH)/src/mdroidplus/apks/$FOLDER_NAME/ -type f -name '*.apk' -exec calabash-android run {} --format html --out $(pwd $PATH)/src/results/screenshots/calabash/$FOLDER_NAME/output.html \;
+
       # Se elimina enlace simbólico a la carpeta del mutante X
       rm $(pwd $PATH)/src/android-builder
       echo $FOLDER_NAME
   fi
 done
+
+# Se elimina enlace simbólico a carpeta de Set de Pruebas de Calabash
+rm $(pwd $PATH)/features
